4c0fcb1de68d0e763f47d796c15f43d3dbb3b332
Kadasumi <kar135sumi@gmail.com>
2022-11-04 02:05:36 +0900

    更新

--- JS/Manunav.js
+++ JS/Manunav.js
@@ -1,11 +1,3 @@
-document.getElementById("hoge").style.display ="none";
-
-function Tops(){
-  var change = document.getElementById("hoge");
-
-  if(change.style.display=="block"){
-    change.style.display ="none";
-  }else{
-    change.style.display ="block";    
-  }
-}
+window.addEventListener("DOMContentLoaded", () => {
+  const Games = document.getElementById("Game");
+})
