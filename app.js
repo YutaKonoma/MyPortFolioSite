9a54e50c6eb8937840b4b69bf21353588c8b8d0c
YutaKonoma <89231452+YutaKonoma@users.noreply.github.com>
2023-01-17 12:21:20 +0900

    test

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-Vus.CreateApp({
+Vue.createApp({
   data(){
     return{
-      message: "Hello Vue.js",
-      };
+        isShow: true,
+        isMobile: true,
+        message: "Hello Vue.js",
+        emMsg: "<a>!!!!</a>",
+        isExpanded: false,
+        bloodType: "A",
+        works: ["Game", "Scenario", "Character Design", "Document", "Program"],
+
+        person: {
+            name: "yamada",
+            age:16,
+        },
+        count= 20,
+        };
+    },
+    methods: {
+        countDown() {
+            this.count--;
+            this.count = Math.max(0, this.count);
+        },
     },
   }).mount("#app");
